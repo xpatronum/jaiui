035ff085a664b6de746e278e43b06e4e6a7151f8
Gleb Myznikov <myznikovgleb@yandex.ru>
2025-09-27 20:44:59 +0300

    Add handlers

--- src/shared/mocks/browser.ts
+++ src/shared/mocks/browser.ts
@@ -1,5 +1,9 @@
 import { setupWorker } from "msw/browser";
 
-import { handlersTS, handlersUser } from "./handlers";
+import { handlersReports, handlersTS, handlersUser } from "./handlers";
 
-export const worker = setupWorker(...handlersUser, ...handlersTS);
+export const worker = setupWorker(
+  ...handlersUser,
+  ...handlersTS,
+  ...handlersReports,
+);
